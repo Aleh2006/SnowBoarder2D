eeb047414790a754df9ddc41025710572b5e7f51
Oleg <60525788+Aleh2006@users.noreply.github.com>
2024-01-16 20:05:22 -0800

    Update CrashDetector.cs

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,20 +6,19 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    //[SerializeField] float loadDelay = 0.5f;
-    //[SerializeField] ParticleSystem crashEffect;
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
-            // crashEffect.Play();
-            // Invoke("ReloadScene", loadDelay);
-            Debug.Log("uch");
+             crashEffect.Play();
+             Invoke("ReloadScene", loadDelay);
         }
     }
 
-   // void ReloadScene()
-   // {
-       // SceneManager.LoadScene(0);
-  //  }
+     void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+      }
 }
