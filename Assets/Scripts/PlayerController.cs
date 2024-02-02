ff91471ee8b2257894d8d2e7c462a10dd2aa1d43
Kewka-Tea <yura30151@inbox.ru>
2024-02-02 23:08:31 +0300

    fixed jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,46 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] bool isGrounded;
     Rigidbody2D rb2d;
+    Collider2D collider2d;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<Collider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
 
-      else  if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
+
+        // Check if the player is grounded
+        //isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+        isGrounded = Physics2D.Raycast(collider2d.bounds.center, Vector2.down, collider2d.bounds.extents.y + 0.1f, LayerMask.GetMask("Ground")); // Используем Physics2D.Raycast
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) // Check if grounded and spacebar is pressed
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
