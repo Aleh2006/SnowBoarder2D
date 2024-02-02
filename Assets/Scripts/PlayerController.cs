a1eedb42ed8998277b984110c6333ecbcb04c886
Oleg <oleg@Olegs-MacBook-Air.local>
2024-02-02 14:47:39 -0800

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,13 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] float jumpForce = 5f;
     [SerializeField] bool isGrounded;
     Rigidbody2D rb2d;
-
+    Collider2D col;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
 
     }
 
@@ -33,7 +34,7 @@ public class NewBehaviourScript : MonoBehaviour
         }
 
         // Check if the player is grounded
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+        isGrounded = Physics2D.IsTouchingLayers(col, LayerMask.GetMask("Ground"));
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space)) // Check if grounded and spacebar is pressed
         {
@@ -43,6 +44,6 @@ public class NewBehaviourScript : MonoBehaviour
 
     void Jump()
     {
-        rb2d.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
     }
 }
