58fb5b7720fd0ce72d2ccf2b61e79ad58e5d7ee3
Oleg <oleg@Olegs-MacBook-Air.local>
2024-02-02 11:42:48 -0800

    Implement jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,28 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] bool isGrounded;
     Rigidbody2D rb2d;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
 
-      else  if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
+
+        // Check if the player is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) // Check if grounded and spacebar is pressed
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb2d.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
     }
 }
